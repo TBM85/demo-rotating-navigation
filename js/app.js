fc0ad82eec1a46df720b713050686e3bd0938ec4
TBM85 <tballesterm@gmail.com>
2021-11-12 08:47:19 +0100

    Home page rotates when hamburger menu is clicked

--- js/app.js
+++ js/app.js
@@ -3,13 +3,15 @@ const btnMenu = document.querySelector(".btn-menu");
 
 let isCrossing = false;
 
-// When the hamburger menu is clicked, the shape toggle between a cross and the original shape
+// When the hamburger menu is clicked, the shape toggle between a cross and the original shape. And the container alternates between rotating or not
 btnMenu.addEventListener("click", () => {
   if (!isCrossing) {
     btnMenu.classList.add("open");
     isCrossing = true;
+    container.classList.add("show");
   } else {
     btnMenu.classList.remove("open");
     isCrossing = false;
+    container.classList.remove("show");
   }
 });
