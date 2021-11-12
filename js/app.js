a1d189bff41a74ae38958d8708e9db66c26b6a0f
TBM85 <tballesterm@gmail.com>
2021-11-12 08:42:32 +0100

    Include an animated hamburger menu

--- js/app.js
+++ js/app.js
@@ -1,12 +1,15 @@
-const openBtn = document.querySelector(".open-btn");
-const closeBtn = document.querySelector(".close-btn");
-
 const container = document.querySelector(".container");
+const btnMenu = document.querySelector(".btn-menu");
 
-openBtn.addEventListener("click", () => {
-  container.classList.add("show");
-})
+let isCrossing = false;
 
-closeBtn.addEventListener("click", () => {
-  container.classList.remove("show");
-})
+// When the hamburger menu is clicked, the shape toggle between a cross and the original shape
+btnMenu.addEventListener("click", () => {
+  if (!isCrossing) {
+    btnMenu.classList.add("open");
+    isCrossing = true;
+  } else {
+    btnMenu.classList.remove("open");
+    isCrossing = false;
+  }
+});
